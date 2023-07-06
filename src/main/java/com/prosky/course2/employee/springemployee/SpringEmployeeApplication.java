96d9c7b9b46e4e54ffb305240f4744a0a9675041
zhannatkachenko <zhanna.kindgirl@gmail.com>
2023-07-06 03:03:14 +0300

    initial

---
+++ src/main/java/com/prosky/course2/employee/springemployee/SpringEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.prosky.course2.employee.springemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEmployeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prosky/course2/employee/springemployee/SpringEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prosky.course2.employee.springemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
