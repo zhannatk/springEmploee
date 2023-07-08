da2e6edc08c002e7e95b723bd2ec43ba8395b9f9
zhannatkachenko <zhanna.kindgirl@gmail.com>
2023-07-08 03:13:46 +0300

    well done

---
+++ src/main/java/com/prosky/course2/employee/springemployee/Exceptions/EmployeeNotFoundException.java
@@ -0,0 +1,11 @@
+package com.prosky.course2.employee.springemployee.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EmployeeNotFoundException extends RuntimeException {
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/prosky/course2/employee/springemployee/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.prosky.course2.employee.springemployee.service;
+
+import com.prosky.course2.employee.springemployee.Employee;
+
+import java.util.Collection;
+
+public interface EmployeeService {
+    Employee addEmployee(String firstName, String lastName);
+
+    Employee removeEmployee(String firstName, String lastName);
+
+    Employee findEmployee(String firstName, String lastName);
+    Collection<Employee> getEmployeesSet();
+}
